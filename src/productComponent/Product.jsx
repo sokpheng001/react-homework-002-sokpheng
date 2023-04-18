1108231f09bb755f79c3b613b8f3174bdc8aaea2
sokpheng001 <115383587+sopi001@users.noreply.github.com>
2023-04-18 20:48:00 +0700

    sokpheng-homework-react

--- src/productComponent/Product.jsx
+++ src/productComponent/Product.jsx
@@ -2,14 +2,8 @@ import ChildCard from './ChildCard';
 import React, { useEffect, useState } from 'react';
 
 
-const name = async()=>{
-  const f = await fetch("https://api.escuelajs.co/api/v1/products");
-  const g = await f.json();
-  console.log(g)
-}
-
 function Product() {
-  name()
+
   const [product, setProduct] = useState([{}])
   useEffect(()=>{
     const dataFect = async ()=>{
